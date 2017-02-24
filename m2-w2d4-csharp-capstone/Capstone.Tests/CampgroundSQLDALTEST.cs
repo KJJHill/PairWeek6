a4bd8a45a32732e387c999dc28bed3ede2aeefe4
Amanda Ronis <amanda.ronis@gmail.com>
2017-02-24 13:51:29 -0500

    Changes as of 1:50pm

--- m2-w2d4-csharp-capstone/Capstone.Tests/CampgroundSQLDALTEST.cs
+++ m2-w2d4-csharp-capstone/Capstone.Tests/CampgroundSQLDALTEST.cs
@@ -34,14 +34,14 @@ namespace Capstone.Tests
 
                 cmd = new SqlCommand(@"SELECT COUNT(*) FROM campground 
                                      JOIN site ON site.campground_id = campground.campground_id 
-                                     WHERE park.park_id = 1
-                                     AND 02 BETWEEN(open_from_mm) AND(open_to_mm)
-                                     AND 03 BETWEEN(open_from_mm) and(open_to_mm)
-									 AND site.site_id NOT IN (SELECT site_id FROM reservation 
-									 WHERE (from_date BETWEEN('2017-2-20') AND ('2017-03-01'))
-                                     OR (to_date BETWEEN('2017-2-20') AND ('2017-03-01'))
-                                     OR (('2017-2-20') BETWEEN from_date AND to_date) 
-                                     OR(('2017-03-01') BETWEEN from_date AND to_date));", connection);
+                                     JOIN reservation ON reservation.site_id = site.site_id 
+                                     WHERE campground.park_id = 1 
+                                     AND 06 BETWEEN (open_from_mm) AND (open_to_mm) 
+                                     AND 06 BETWEEN (open_from_mm) and (open_to_mm) 
+                                     AND(from_date NOT BETWEEN('2017-2-20') AND ('2017-03-01'))
+                                     AND(to_date NOT BETWEEN('2017-2-20') AND ('2017-03-01')
+                                     AND('2017-2-20' NOT BETWEEN (from_date) AND (to_date)) 
+                                     AND('2017-03-01' NOT BETWEEN (from_date) AND (to_date)));", connection);
                 cmd.ExecuteNonQuery();
 
             }
